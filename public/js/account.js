d64f0702b4e941b8ed9cd1586c2f6b847933f74d
Cashmay0r <aidanenseleit98@gmail.com>
2021-06-29 07:18:40 +1000

    Added some basic layout for some pages, not styled yet

--- public/js/account.js
+++ public/js/account.js
@@ -3,7 +3,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	auth();
 	getAuth
 		.then((user) => {
-			console.log('User Logged in');
+			console.log('User is Logged in');
+			if (document.getElementById('welcomeText') != null) {
+				document.getElementById('welcomeText').innerHTML = `Welcome ${user.displayName}!`;
+				document.getElementById('accountContent').removeAttribute('hidden');
+			}
 		})
 		.catch((message) => {
 			console.log('User not logged in');
@@ -43,73 +47,72 @@ document.addEventListener('DOMContentLoaded', (event) => {
 			window.location = 'updateDetails.html';
 		});
 	}
-	//Listener for NavAccount
+	//Get data and append for welcome string
 });
 
 function printUserData(userProfile) {
 	console.log(userProfile);
-	document.getElementById('image').innerHTML += ' ' + userProfile.imageUrl;
-	document.getElementById('userid').innerHTML += ' ' + userProfile.userId;
-	document.getElementById('authlevel').innerHTML += ' ' + userProfile.authLevel;
-	document.getElementById('fName').innerHTML += ' ' + userProfile.fName;
-	document.getElementById('lName').innerHTML += ' ' + userProfile.lName;
-	document.getElementById('email').innerHTML += ' ' + userProfile.email;
-	document.getElementById('phone').innerHTML += ' ' + userProfile.phone;
-	document.getElementById('company').innerHTML += ' ' + userProfile.company;
-	document.getElementById('companyid').innerHTML += ' ' + userProfile.companyId;
+	//document.getElementById('image').innerHTML += ' ' + userProfile.imageUrl;
+	//document.getElementById('userid').innerHTML += ' ' + userProfile.userId;
+	document.getElementById('authlevel').innerHTML += ' ' + 'Account Type: ' + userProfile.authLevel;
+	document.getElementById('displayName').innerHTML += ' ' + userProfile.fName + ' ' + userProfile.lName;
+	document.getElementById('email').innerHTML += ' ' + 'Email: ' + userProfile.email;
+	document.getElementById('phoneNum').innerHTML += ' ' + 'Phone: ' + userProfile.phone;
+	document.getElementById('companyName').innerHTML += ' ' + userProfile.company;
+	document.getElementById('companyId').innerHTML += ' ' + userProfile.companyId;
+	document.getElementById('bio').innerHTML += ' ' + 'This is my Account Yo wowzas';
 }
 
 function writeUserData() {
-	const user = firebase.auth().currentUser;
-	console.log(user);
-	let authLevel = 'User';
-	const email = user.email;
-	const userId = user.uid;
-	const img = document.getElementById('imageCreate').value;
-	const fName = document.getElementById('fNameCreate').value;
-	const lName = document.getElementById('lNameCreate').value;
-	const phone = document.getElementById('phoneCreate').value;
-	const company = document.getElementById('companyCreate').value;
-	const companyId = document.getElementById('companyIdCreate').value;
+	auth();
+	getAuth.then((user) => {
+		console.log(user);
+		let authLevel = 'User';
+		const email = user.email;
+		const userId = user.uid;
+		const img = document.getElementById('imageCreate').value;
+		const fName = document.getElementById('fNameCreate').value;
+		const lName = document.getElementById('lNameCreate').value;
+		const phone = document.getElementById('phoneCreate').value;
+		const company = document.getElementById('companyCreate').value;
+		const companyId = document.getElementById('companyIdCreate').value;
+		const displayName = fName + ' ' + lName;
 
-	firebase
-		.database()
-		.ref('users/' + userId)
-		.set({
-			imageUrl: img,
-			userId: userId,
-			authLevel: authLevel,
-			fName: fName,
-			lName: lName,
-			email: email,
-			phone: phone,
-			company: company,
-			companyId: companyId,
-		})
-		.then((result) => {
-			console.log('Uploaded');
-			window.location = '../html/home.html';
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-}
-//TODO: Need to fix this feature and make it useful
-function updateUserInfo() {
-	var user = firebase.auth().currentUser;
-	console.log(user.uid);
-
-	user
-		.updateProfile({
-			displayName: 'Aidaaan',
-			photoURL: 'https://example.com/jane-q-user/profile.jpg',
-		})
-		.then(function () {
-			console.log('User updated ' + user.displayName);
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
+		firebase
+			.database()
+			.ref('users/' + userId)
+			.set({
+				imageUrl: img,
+				userId: userId,
+				authLevel: authLevel,
+				fName: fName,
+				lName: lName,
+				email: email,
+				phone: phone,
+				company: company,
+				companyId: companyId,
+			})
+			.then((result) => {
+				console.log('Uploaded');
+				window.location = '../html/home.html';
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+		//Updating User profile used in auth with relevant data
+		user
+			.updateProfile({
+				displayName: displayName,
+				photoURL: null,
+				phoneNumber: phone,
+			})
+			.then(function () {
+				console.log('Update User Profile: ' + user.displayName);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	});
 }
 
 function updateDetails() {
