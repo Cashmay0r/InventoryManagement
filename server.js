5619fe3380ce06d7024c35e0a1514e1f30a78647
Cashmay0r <aidanenseleit98@gmail.com>
2021-06-29 10:27:49 +1000

    Made new development branch

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from "express";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
 const port = 3000;
@@ -8,16 +8,16 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use(express.static(__dirname + "/public/"));
-app.use(express.static(__dirname + "/public/html/"));
-app.use(express.static(__dirname + "/public/css/"));
-app.use(express.static(__dirname + "/public/js/"));
+app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/public/html/'));
+app.use(express.static(__dirname + '/public/css/'));
+app.use(express.static(__dirname + '/public/js/'));
 
-app.get("/", (req, res) => {
-  res.send("Login Page");
-  res.sendFile(__dirname + "/public/html/index.html");
+app.get('/', (req, res) => {
+	res.send('Login Page');
+	res.sendFile(__dirname + '/public/html/index.html');
 });
 
 app.listen(port, () => {
-  console.log(`Server Listening at http://localhost:${port}`);
+	console.log(`Server Listening at http://localhost:${port}`);
 });
